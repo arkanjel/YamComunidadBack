2953de7e0667a393d8f925eadc39b4e3244c2609
Ivo <ivo.e@hotmail.es>
2025-11-24 19:54:24 -0300

    actualizado para vercel

--- server.js
+++ server.js
@@ -28,4 +28,6 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
+// app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
+// ✔️ AGREGAR ESTO
+export default app;
